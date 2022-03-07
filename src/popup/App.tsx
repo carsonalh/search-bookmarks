5dc6e26267fb12ff07e87281795f5f797170c4b8
Carson Holloway <carsonalh@gmail.com>
2022-03-07 15:12:38 +1000

    Add feature to select and follow result
    Add feature to highlight the selected result. Add feature to open a new
    tab with the url of the selected result. Add keyboard shortcuts <Up>,
    <Down>, and <Enter> to utilise this functionality.

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -7,20 +7,15 @@ const App = () => {
   const [searchResults, setSearchResults] = useState<
     chrome.bookmarks.BookmarkTreeNode[]
   >([]);
+  const [selectedResult, setSelectedResult] = useState<number>(0);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     // We'll go ahead and make the trie here later
 
-    // For now, we'll just get all the bookmarks
-    // const getRootBookmark = async () => {
-    //   const [root] = await chrome.bookmarks.getTree();
-    //   if (!root || !root.children) {
-    //     console.error("The root or it's children was nullish. I give up.");
-    //   }
-    //   return root;
-    // };
+    // We could do this with async/await but something breaks when I try that
+    // (idk what?)
 
-    // getRootBookmark().then(setBookmarks);
     // For now, we'll just get all the bookmarks
     chrome.bookmarks.getTree(([root]) => {
       if (!root || !root.children) {
@@ -29,6 +24,15 @@ const App = () => {
       setBookmarks(root);
     });
   }, []);
+
+  // Focus the input on popup
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+
+    inputRef.current.focus();
+  }, [inputRef.current]);
 
   useEffect(() => {
     if (bookmarks === null) {
@@ -55,26 +59,77 @@ const App = () => {
     const results = array.filter(
       (bookmark) =>
         searchTerm !== "" &&
+        bookmark.url !== undefined &&
         bookmark.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     setSearchResults(results);
   }, [searchTerm]);
 
+  const selectOffset = (offset: number) => {
+    if (searchResults.length === 0) {
+      setSelectedResult(0);
+    } else {
+      const newResult = Math.max(
+        0,
+        Math.min(selectedResult + offset, searchResults.length - 1)
+      );
+      setSelectedResult(newResult);
+    }
+  };
+
+  const selectUp = () => selectOffset(-1);
+  const selectDown = () => selectOffset(1);
+
+  const gotoSelected = () => {
+    const bookmark = searchResults[selectedResult];
+    if (!bookmark) {
+      return;
+    }
+    const title = searchResults[selectedResult].title;
+    console.debug(`Opening bookmark "${title}"`);
+    const url = searchResults[selectedResult].url;
+    chrome.tabs.create({
+      url: url,
+    });
+  };
+
+  const keyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    console.debug(e.key);
+    const listeningFor = ["ArrowUp", "ArrowDown", "Enter"];
+    if (listeningFor.includes(e.key)) {
+      e.preventDefault();
+
+      if (e.key === "ArrowUp") {
+        selectUp();
+      } else if (e.key === "ArrowDown") {
+        selectDown();
+      } else if (e.key === "Enter") {
+        gotoSelected();
+      }
+    }
+  };
+
   return (
     <>
       <input
+        ref={inputRef}
+        onKeyDown={keyDown}
         type="text"
         placeholder="Search Bookmarks..."
         onChange={(e) => setSearchTerm(e.target.value)}
         value={searchTerm}
       />
-      <p>Your search term: {searchTerm}</p>
-      <p>Results:</p>
       <ul>
-        {searchResults.map((bookmark) => (
-          <li key={bookmark.id}>{bookmark.title}</li>
-        ))}
+        {searchResults.map((bookmark, i) =>
+          selectedResult === i ? (
+            <li style={{ color: "red" }} key={bookmark.id}>
+              {bookmark.title}
+            </li>
+          ) : (
+            <li key={bookmark.id}>{bookmark.title}</li>
+          )
+        )}
       </ul>
     </>
   );
