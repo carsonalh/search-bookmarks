9d63950c575f3517e2e74a99a4895f4b98fba92a
Carson Holloway <carsonalh@gmail.com>
2022-03-13 20:55:29 +1000

    Removed unnecessary permissions in manifest
    Also remove redundant comments.

--- src/background.ts
+++ src/background.ts
@@ -7,27 +7,4 @@
  * allowed) for reading when searched.
  */
 
-chrome.runtime.onInstalled.addListener(() => {
-  console.info("Extension has been installed!");
-});
-
-// let color = "#3aa757";
-
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.sync.set({ color });
-//   console.log("Default background color set to %cgreen", `color: ${color}`);
-// });
-
-// const ONE_SECOND = 1 / 60;
-
-// chrome.alarms.create("checker.main", {
-//   periodInMinutes: ONE_SECOND * 2,
-// });
-
-// chrome.alarms.onAlarm.addListener((alarm) => {
-//   if (alarm.name === "checker.main") {
-//     console.debug("The usual alarm is running as it should...");
-//   } else {
-//     console.warn("A foreign alarm has been called!");
-//   }
-// });
+export {};
